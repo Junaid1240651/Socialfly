ad1031ba597289940298e5a178f144998fa396a9
Junaid <jk1240651@gmail.com>
2024-03-29 17:08:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       "/api": {
         target: "https://socialfly-two.vercel.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
